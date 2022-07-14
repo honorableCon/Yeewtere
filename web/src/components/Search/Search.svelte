d26ed865f693faebf978a5cbe93427a13adb14e8
Honorable Con <34490054+honorableCon@users.noreply.github.com>
2022-07-14 15:00:02 +0000

    Initial commit

---
+++ web/src/components/Search/Search.svelte
@@ -0,0 +1,61 @@
+<script>
+  import AutoComplete from "simple-svelte-autocomplete"
+
+  export let data
+
+  const searchPlaceholders = {
+		"Anglais": "Search a word",
+		"Français": "Cherche un mot",
+		"Wolof": "Saytu ben baat"
+	}
+	
+  let selectedwaxtane = null
+  let lang = "Wolof"
+
+  const forceUpdate = async (_) => {};
+
+  function toggle(event) {
+		lang = event.target.innerHTML;
+	}
+
+</script>
+
+<h1>Search in {lang}</h1>
+
+<div>
+  <button on:click={toggle}>
+	  Wolof
+  </button>
+  <button on:click={toggle}>
+    Français
+  </button>
+  <button on:click={toggle}>
+    Anglais
+  </button>
+</div>
+
+<style>
+  table {
+    width: 100%;
+  }
+</style>
+
+{#await forceUpdate(lang) then _}
+  <AutoComplete items="{data}" bind:selectedItem="{selectedwaxtane}" bind:labelFieldName={lang} placeholder="{searchPlaceholders[lang]}" className="search" inputClassName="search-input" />
+  {#if selectedwaxtane}
+    <table>
+      <tr>
+        <th>N°</th>
+        <th>Wolof</th>
+        <th>Français</th>
+        <th>Anglais</th>
+      </tr>
+      <tr>
+        <td>{selectedwaxtane['N°']}</td>
+        <td>{selectedwaxtane.Wolof}</td>
+        <td>{selectedwaxtane.Français}</td>
+        <td>{selectedwaxtane.Anglais}</td>
+      </tr>
+    </table>
+  {/if}
+{/await}
